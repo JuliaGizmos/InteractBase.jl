71bc032624677f7fd5a7c01c50fc55e7f9a37cf5
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-05-13 14:04:22 +0100

    preliminary support for radio and togglebuttons

--- src/optioninput.jl
+++ src/optioninput.jl
@@ -13,3 +13,42 @@ function dropdown(::WidgetTheme, options, o = nothing; postprocess = identity, k
     primary_obs!(ui, "value")
     slap_design!(ui)
 end
+
+#TODO: check interactnext API and match it!
+function radiobuttons(T::WidgetTheme, options;
+    postprocess = identity, kwargs...)
+
+    value = Observable{eltype(options)}(options[1])
+    s = gensym()
+    btns = [(dom"input[name = $s, type=radio, v-model=value, value=$option]"(),
+        dom"label"(option), dom"br"()) for option in options]
+
+    template = dom"form"(
+        Iterators.flatten(btns)...
+    )
+    ui = vue(template, ["value" => value])
+    primary_obs!(ui, "value")
+    slap_design!(ui)
+end
+
+function togglebuttons(T::WidgetTheme, options;
+    postprocess = identity, kwargs...)
+
+    jfunc = js"""function (ev){
+        return this.value = ev;
+    }
+    """
+
+    value = Observable("")
+
+    s = gensym()
+    btns = [(dom"button[name = $s]"(attributes=Dict("v-on:click"=>"changeValue('$option')")),
+        dom"label"(option), dom"br"()) for option in options]
+
+    template = dom"div"(
+        Iterators.flatten(btns)...
+    )
+    ui = vue(template, ["value" => value], methods = Dict(:changeValue => jfunc))
+    primary_obs!(ui, "value")
+    slap_design!(ui)
+end
