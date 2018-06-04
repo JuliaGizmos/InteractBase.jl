3742731b72150e0c4491839e393afff188b4a1ab
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-06-04 18:39:44 +0100

    tests

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,7 @@
 using NodeJS
-
-cd(joinpath(dirname(@__FILE__), "..", "assets", "npm")) do
+npm_path = joinpath(dirname(@__FILE__), "..", "assets", "npm")
+mkpath(npm_path)
+cd(npm_path) do
     npm = NodeJS.npm_cmd()
     run(`$npm install -y`)
     run(`$npm install -y katex`)
