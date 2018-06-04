7f15463ed03e30e394d9096f8172d9b2f9fd3860
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-06-04 17:19:06 +0100

    update scope

--- src/output.jl
+++ src/output.jl
@@ -1,21 +1,22 @@
 using WebIO, JSExpr
 
 function latex(txt)
+   (txt isa Observable) || (txt = Observable(txt))
    w = Scope(imports=[
-           "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.10.0-alpha/katex.min.js",
-           "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.10.0-alpha/katex.min.css"])
+           "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.js",
+           "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.css"])
 
-   w["text"][] = txt
+   w["value"] = txt
 
    onimport(w, @js function (k)
        this.k = k
        this.container = this.dom.querySelector("#container")
-       k.render($txt, this.container)
+       k.render($(txt[]), this.container)
    end)
 
-   onjs(w["text"], @js (txt) -> this.k.render(txt, this.container))
+   onjs(w["value"], @js (txt) -> this.k.render(txt, this.container))
 
    w.dom = dom"div#container"()
 
-   w
+   Widget(Val{:latex}(), w, "value")
 end
