7312edb3d86df65d67b8b2da28b5aaa523f6fada
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-07-11 00:46:09 +0100

    docs for widget

--- src/manipulate.jl
+++ src/manipulate.jl
@@ -65,6 +65,30 @@ macro manipulate(expr)
     end
 end
 
+"""
+`widget(args...; kwargs...)`
+
+Automatically convert Julia types into appropriate widgets. `kwargs` are passed to the
+more specific widget function.
+
+## Examples
+
+```julia
+map(display, [
+    widget(1:10),                 # Slider
+    widget(false),                # Checkbox
+    widget("text"),               # Textbox
+    widget(1.1),                  # Spinbox
+    widget([:on, :off]),          # Toggle Buttons
+    widget(Dict("π" => float(π), "τ" => 2π)),
+    widget(colorant"red"),        # Color picker
+    widget(Dates.today()),        # Date picker
+    widget(Dates.Time()),         # Time picker
+    ]);
+```
+"""
+function widget end
+
 @deprecate widget(x, label) widget(x, label = label)
 
 widget(x; kwargs...) = x
