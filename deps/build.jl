ba7297b72fee3e5fe49c4624a6e4271965725257
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-08-30 12:35:54 +0100

    fixed nouislider dependencies (#105)

--- deps/build.jl
+++ deps/build.jl
@@ -8,8 +8,8 @@ deps = [
     "https://use.fontawesome.com/releases/v5.0.7/js/all.js",
     "https://raw.githubusercontent.com/piever/InteractResources/master/highlight/prism.css",
     "https://raw.githubusercontent.com/piever/InteractResources/master/highlight/prism.js",
-    "https://cdn.bootcss.com/noUiSlider/11.1.0/nouislider.min.js",
-    "https://cdn.bootcss.com/noUiSlider/11.1.0/nouislider.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/11.1.0/nouislider.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/11.1.0/nouislider.min.css",
     "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.js",
     "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.css"
 ]
