74ce834afaa927943c4784d1b108091b4447da9d
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-10-01 19:31:45 +0100

    docs

--- src/utils.jl
+++ src/utils.jl
@@ -43,6 +43,19 @@ function triggeredby(a::AbstractObservable{T}, b::AbstractObservable) where {T}
     s
 end
 
+"""
+`onchange(w::AbstractWidget, change = w[:changes])`
+
+Return a widget that's identical to `w` but only updates on `change`. For a slider it corresponds to releasing it
+and for a textbox it corresponds to losing focus.
+
+## Examples
+
+```julia
+sld = slider(1:100) |> onchange # update on release
+txt = textbox("Write here") |> onchange # update on losing focuse
+```
+"""
 function onchange(w::AbstractWidget{T}, change = w[:changes]) where T
     o = triggeredby(w, change)
     Widget{T}(w, output = o)
