840cd39638c04b0e2ffd10ee5a73951adccc9f5d
autodocs <autodocs>
2018-12-30 20:36:42 +0000

    build based on 8e7e7d0

---
+++ v0.8.2/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#InteractBase-1",
+    "page": "Introduction",
+    "title": "InteractBase",
+    "category": "section",
+    "text": "This package is a developer target and it includes the implementation of Interact widgets.Documentation for Interact is available here"
+},
+
+]}
