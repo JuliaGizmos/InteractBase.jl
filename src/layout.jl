fe68645c43523e9e66ba7a1e842df0467c106652
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-02-05 17:40:47 +0000

    fix manipulatelayout

--- src/layout.jl
+++ src/layout.jl
@@ -2,4 +2,4 @@ center(w) = flex_row(w)
 center(w::Widget) = w
 center(w::Widget{:toggle}) = flex_row(w)
 
-manipulatelayout(::WidgetTheme) = t -> node(:div, map(center, values(components(t)))..., map(center, output))
+manipulatelayout(::WidgetTheme) = t -> node(:div, map(center, values(components(t)))..., map(center, t.output))
