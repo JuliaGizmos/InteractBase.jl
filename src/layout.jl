db20a559ca12d84b55ffd449c619539f0ca95ba2
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-02-05 18:02:13 +0000

    add fallback for observable

--- src/layout.jl
+++ src/layout.jl
@@ -3,3 +3,11 @@ center(w::Widget) = w
 center(w::Widget{:toggle}) = flex_row(w)
 
 manipulatelayout(::WidgetTheme) = t -> node(:div, map(center, values(components(t)))..., map(center, t.output))
+
+function widget(::WidgetTheme, x::Observable; label = nothing)
+    if label === nothing
+        x
+    else
+        Widget{:observable}(["label" => label], output = x, layout = t -> flex_row(t["label"], t.output))
+    end
+end
