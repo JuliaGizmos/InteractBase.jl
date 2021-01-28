bae8fd23daac3ec8fa631cbf9566fb82289c81ea
TheCedarPrince <jacobszelko@gmail.com>
2021-01-28 04:56:45 -0500

    Fix default value in docstring and added example (#159)

--- src/optioninput.jl
+++ src/optioninput.jl
@@ -231,11 +231,11 @@ radiobuttons(T::WidgetTheme, vals; kwargs...) =
 """
 ```
 checkboxes(options::AbstractDict;
-         value = first(values(options)))
+         value = valtype(options)[]
 ```
 
 A list of checkboxes whose item labels are the keys of options.
-Tthe observable will hold an array containing the values
+The observable will hold an array containing the values
 of all selected items,
 e.g. `checkboxes(OrderedDict("good"=>1, "better"=>2, "amazing"=>9001))`
 
@@ -264,6 +264,17 @@ Note that the `options` can be modified from the widget directly:
 ```julia
 wdg[:options][] = ["c", "d", "e"]
 ```
+
+To create checkboxes that are already checked off when shown, one can use the `value` keyword:
+
+```julia
+options = ["a", "b", "c"]
+value = ["a", "c"]
+checkboxes(options, value = value)
+```
+
+The boxes "a" and "c" will be checked off when the checkboxes widget is shown.
+Both `options` and `value` can be Observables.
 """
 checkboxes(T::WidgetTheme, options; kwargs...) =
     Widget{:checkboxes}(multiselect(T, options; typ="checkbox", kwargs...))
