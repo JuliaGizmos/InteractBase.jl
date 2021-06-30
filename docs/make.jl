e4cee059a6d65a06918590214b2940dfc8612760
piever <pietro.vertechi@protonmail.com>
2021-06-30 17:17:48 +0200

    update docs script

--- docs/make.jl
+++ docs/make.jl
@@ -11,11 +11,4 @@ makedocs(
     ]
 )
 
-deploydocs(
-    repo = "github.com/piever/InteractBase.jl.git",
-    target = "build",
-    julia  = "1.0",
-    osname = "linux",
-    deps   = nothing,
-    make   = nothing
-)
+deploydocs(repo = "github.com/piever/InteractBase.jl.git")
