5a4b69c58438c3ac96cc06c42065c335fc86ce63
Documenter.jl <documenter@juliadocs.github.io>
2021-06-30 15:25:26 +0000

    build based on 2ede9dc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#InteractBase","page":"Introduction","title":"InteractBase","text":"","category":"section"},{"location":"","page":"Introduction","title":"Introduction","text":"This package is a developer target and it includes the implementation of Interact widgets.","category":"page"},{"location":"","page":"Introduction","title":"Introduction","text":"Documentation for Interact is available here","category":"page"}]
+}
